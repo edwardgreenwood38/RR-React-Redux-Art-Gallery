2a28db706010ecd81bf9921768c6a0bc85ef2939
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-03-07 18:06:13 -0500

    app comments

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import { useSelector, useDispatch, connect } from 'react-redux'
 import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice'
 import { useEffect } from 'react';
 
+
 function App(props) {
   const dispatch = useDispatch()
   const data = useSelector((state) => state.data)
 
+
+  // identify space for image
   const renderImg = () => {
     if(data.apiData) {
       return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
     } else {
       return <p>image here</p>
     }
-  }
+  };
+
 
   useEffect(() => {
     dispatch(fetchData())
   }, [props.objectId, dispatch])
 
 
+  //  display output
   return (
     <div className="App">
       <div>
@@ -37,9 +42,10 @@ function App(props) {
       </div>
     </div>
   );
-}
+};
 
 
+// export
 const mapStateToProps = (state, ownProps) => ({ objectId: state.data.objectId })
 
 export default connect(mapStateToProps)(App);
