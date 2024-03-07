cc1cf7991bed4f21015fc1b062760953be1ccd05
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-03-07 18:07:04 -0500

    store updates

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import dataReducer from './features/dataSlice'
 import { logger } from './features/middleware'
 
+
+// setup the store
 export const store = configureStore({
     reducer: {
         data: dataReducer
